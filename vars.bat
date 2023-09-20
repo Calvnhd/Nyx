a8fad11e0443369acd44f4b2b46c1db57f2b4840
Calvin Davidson <calvinhdavidson@gmail.com>
2023-09-20 23:34:43 +1000

    import ship, basic scripting

--- vars.bat
+++ vars.bat
@@ -6,6 +6,5 @@ set PROJECT_DIR=%ROOTDIR%
 set UPROJECT_PATH=%PROJECT_DIR%\%PROJECT%.uproject
 
 set UE50_DIR=C:\Program Files\Epic Games\UE_5.0
+set UE5EDITOR_EXE=%UE50_DIR%\Engine\Binaries\Win64\UnrealEditor.exe
 set BUILD_BAT=%UE50_DIR%\Engine\Build\BatchFiles\Build.bat
-
-call "%BUILD_BAT%" %PROJECT%Editor Win64 Development "%UPROJECT_PATH%" -waitmutex -NoHotReload 
