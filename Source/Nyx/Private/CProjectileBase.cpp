5d211e4bdeb2ed6ae4d602cdceea31282c213c73
Calvin Davidson <calvinhdavidson@gmail.com>
2023-10-15 15:52:11 +1100

    begin adding projectile code and assets

--- Source/Nyx/Private/CProjectileBase.cpp
+++ Source/Nyx/Private/CProjectileBase.cpp
@@ -12,107 +12,115 @@
 // Sets default values
 ACProjectileBase::ACProjectileBase()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
+	// Create components
 	ImpactVFX = CreateDefaultSubobject<UParticleSystem>("ImpactVFX");
 	SphereComp = CreateDefaultSubobject<USphereComponent>("SphereComp");
 	EffectComp = CreateDefaultSubobject<UParticleSystemComponent>("EffectComp");
+	MovementComp = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovementComp");
 
+	// Attachments and hierachy
 	RootComponent = SphereComp;
+	EffectComp->SetupAttachment(SphereComp);
+
+	// Collision
 	// todo: set up "Projectile" collision profile
 	// SphereComp->SetCollisionProfileName("Projectile");
 
-	EffectComp->SetupAttachment(SphereComp);
-
-	MovementComp = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovementComp");
+	// Movement
 	MovementComp->bRotationFollowsVelocity = true;
 	MovementComp->bInitialVelocityInLocalSpace = true;
 	MovementComp->ProjectileGravityScale = 0.0f;
-	MovementComp->InitialSpeed = 500.0f;
+	// This is low, so the override in inherited class is obvious
+	MovementComp->InitialSpeed = 250.0f;
+
+	// todo: audio
 }
 
-void ACProjectileBase::OnProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor,
-									   UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+// void ACProjectileBase::OnProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor,
+// 									   UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 
-{
-	float Radius = 50.0f;
-	float Segments = 32;
-	FColor LineColor = Hit.bBlockingHit ? FColor::Green : FColor::Red;
-	float Lifetime = 5.0f;
-	DrawDebugSphere(GetWorld(), Hit.ImpactPoint, Radius, Segments, LineColor, false, Lifetime);
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(-1, 15.0f, LineColor, TEXT("Projectile Hit"));
-	}
+// {
+// 	float Radius = 50.0f;
+// 	float Segments = 32;
+// 	FColor LineColor = Hit.bBlockingHit ? FColor::Green : FColor::Red;
+// 	float Lifetime = 5.0f;
+// 	DrawDebugSphere(GetWorld(), Hit.ImpactPoint, Radius, Segments, LineColor, false, Lifetime);
+// 	if (GEngine)
+// 	{
+// 		GEngine->AddOnScreenDebugMessage(-1, 15.0f, LineColor, TEXT("Projectile Hit"));
+// 	}
 
-	/*
-	Delegate for notification of blocking collision against a specific component.
-	NormalImpulse will be filled in for physics-simulating bodies, but will be zero for swept-component blocking
-	collisions
+// 	/*
+// 	Delegate for notification of blocking collision against a specific component.
+// 	NormalImpulse will be filled in for physics-simulating bodies, but will be zero for swept-component blocking
+// 	collisions
 
-	DECLARE_DYNAMIC_MULTICAST_SPARSE_DELEGATE_FiveParams(
-	FComponentHitSignature, UPrimitiveComponent, OnComponentHit, UPrimitiveComponent*, HitComponent, AActor*,
-	OtherActor, UPrimitiveComponent*, OtherComp, FVector, NormalImpulse, const FHitResult&, Hit);
+// 	DECLARE_DYNAMIC_MULTICAST_SPARSE_DELEGATE_FiveParams(
+// 	FComponentHitSignature, UPrimitiveComponent, OnComponentHit, UPrimitiveComponent*, HitComponent, AActor*,
+// 	OtherActor, UPrimitiveComponent*, OtherComp, FVector, NormalImpulse, const FHitResult&, Hit);
 
-	Called when a component hits (or is hit by) something solid (e.g. via movement, using Set Location with 'sweep'
-	enabled, or physics simulation).
+// 	Called when a component hits (or is hit by) something solid (e.g. via movement, using Set Location with 'sweep'
+// 	enabled, or physics simulation).
 
-	For collisions during physics simulation to generate hit events, 'Simulation Generates Hit Events' must be enabled
-	for this component.
+// 	For collisions during physics simulation to generate hit events, 'Simulation Generates Hit Events' must be enabled
+// 	for this component.
 
-	When receiving a hit from another object's movement, the directions of 'Hit.Normal' and 'Hit.ImpactNormal'
-	will be adjusted to indicate force from the other object against this object.
+// 	When receiving a hit from another object's movement, the directions of 'Hit.Normal' and 'Hit.ImpactNormal'
+// 	will be adjusted to indicate force from the other object against this object.
 
-	NormalImpulse will be filled in for physics-simulating bodies, but will be zero for swept-component blocking
-	collisions.
-	 */
+// 	NormalImpulse will be filled in for physics-simulating bodies, but will be zero for swept-component blocking
+// 	collisions.
+// 	 */
 
-	// Currently we just have one projectile type so we'll keep the behaviour here
-	// Eventually will want to subclass ProjectileBase and override this behaviour
+// 	// Currently we just have one projectile type so we'll keep the behaviour here
+// 	// Eventually will want to subclass ProjectileBase and override this behaviour
 
-	// Check there's a valid OtherActor and it's not the actor who spawned this projectile (no hitting ourselves)
-	if (OtherActor && OtherActor != GetInstigator())
-	{
-		// Check if what we just hit has an AttributeComponent using casting -- Cast<ExpectedType>(ThingToCast)
-		// GetComponentByClass iterates through actor until it finds the first instance of specified class
-		// StaticClass() lets us easily pass around the class type
+// 	// Check there's a valid OtherActor and it's not the actor who spawned this projectile (no hitting ourselves)
+// 	if (OtherActor && OtherActor != GetInstigator())
+// 	{
+// 		// Check if what we just hit has an AttributeComponent using casting -- Cast<ExpectedType>(ThingToCast)
+// 		// GetComponentByClass iterates through actor until it finds the first instance of specified class
+// 		// StaticClass() lets us easily pass around the class type
 
-		// todo: check if searching for the base class like this will find subclasses too.  ChatGPT suggests using
-		// dynamic_cast? But I'm not sure that's necessary
-		// AND, if I'm overriding this function in the child class, I hope the child version is still going to called,
-		// even though it's type Base?
-		// Or maybe an interface would be better here?
+// 		// todo: check if searching for the base class like this will find subclasses too.  ChatGPT suggests using
+// 		// dynamic_cast? But I'm not sure that's necessary
+// 		// AND, if I'm overriding this function in the child class, I hope the child version is still going to called,
+// 		// even though it's type Base?
+// 		// Or maybe an interface would be better here?
 
-		// Has the actor we've hit implemented an Attribute interface?
-		// Note the U prefix here
-		if (OtherActor->Implements<UCAttributeInterface>())
-		{
-			// Note the I prefix here
-			// Add Execute_ prefix to function call
-			// First argument is the object to call this function on, followed by the function params themselves
-			ICAttributeInterface::Execute_ApplyDamage(OtherActor, DamageAmount);
-		}
+// 		// Has the actor we've hit implemented an Attribute interface?
+// 		// Note the U prefix here
+// 		if (OtherActor->Implements<UCAttributeInterface>())
+// 		{
+// 			// Note the I prefix here
+// 			// Add Execute_ prefix to function call
+// 			// First argument is the object to call this function on, followed by the function params themselves
+// 			ICAttributeInterface::Execute_ApplyDamage(OtherActor, DamageAmount);
+// 		}
 
-		Explode();
-	}
-}
+// 		Explode();
+// 	}
+// }
 
 void ACProjectileBase::Explode()
 {
 	// todo: add ensure(IsValid(this)) to make sure we're valid and aren't already being destroyed
-	if (ImpactVFX)
+	if (ensure(IsValid(this)))
 	{
-		UGameplayStatics::SpawnEmitterAtLocation(this, ImpactVFX, GetActorLocation(), GetActorRotation());
+		if (ImpactVFX)
+		{
+			UGameplayStatics::SpawnEmitterAtLocation(this, ImpactVFX, GetActorLocation(), GetActorRotation());
+		}
+		Destroy();
 	}
-	Destroy();
 }
 
 void ACProjectileBase::PostInitializeComponents()
 {
 	Super::PostInitializeComponents();
 
-	SphereComp->OnComponentHit.AddDynamic(this, &ACProjectileBase::OnProjectileHit);
+	// Delegate bindings
+	//SphereComp->OnComponentHit.AddDynamic(this, &ACProjectileBase::OnProjectileHit);
 }
 
 // Called when the game starts or when spawned
@@ -121,8 +129,9 @@ void ACProjectileBase::BeginPlay()
 	Super::BeginPlay();
 }
 
+// Don't think we need this??
 // Called every frame
-void ACProjectileBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-}
+// void ACProjectileBase::Tick(float DeltaTime)
+// {
+// 	Super::Tick(DeltaTime);
+// }
--- Source/Nyx/Public/CProjectileBase.h
+++ Source/Nyx/Public/CProjectileBase.h
@@ -14,7 +14,8 @@ class UParticleSystemComponent;
 /*
  * Base projectile class for ranged attacks (both player and AI)
  */
-UCLASS()
+// ABSTRACT marks as incomplete, keeping this out of certain dropdown windows like SpawnActor in Unreal
+UCLASS(ABSTRACT) 
 class NYX_API ACProjectileBase : public AActor
 {
 	GENERATED_BODY()
@@ -26,27 +27,27 @@ protected:
 	virtual void BeginPlay() override;
 	virtual void PostInitializeComponents() override;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Effects")
-	TObjectPtr<UParticleSystem> ImpactVFX;
+	// Root component for collisions (and everything else)
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	TObjectPtr<USphereComponent> SphereComp;
+	// Movement
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	TObjectPtr<UProjectileMovementComponent> MovementComp;
+	// Aesthetics
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	TObjectPtr<UParticleSystemComponent> EffectComp;
+	// Explosion effects on hit
+	UPROPERTY(EditDefaultsOnly, Category = "Effects")
+	TObjectPtr<UParticleSystem> ImpactVFX;
 
 	// Mark as BlueprintCallable as needed
-	UFUNCTION()
-	virtual void OnProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
-								 FVector NormalImpulse, const FHitResult& Hit);
+	//UFUNCTION()
+	//virtual void OnProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	//							 FVector NormalImpulse, const FHitResult& Hit);
 
 	UFUNCTION()
 	void Explode();
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	float DamageAmount;
-
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	//UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	//float DamageAmount;
 };
