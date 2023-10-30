e2cddf530a60e4b84edb0b2b435353b259fe704e
Calvin Davidson <calvinhdavidson@gmail.com>
2023-10-30 22:31:45 +1100

    add EnemyBase class

---
+++ Source/Nyx/Private/CEnemyBase.cpp
@@ -0,0 +1,40 @@
+// Copyright (C) 2023 - Calvin Davidson
+
+#include "CEnemyBase.h"
+#include "CAttributeComponent.h"
+#include "CCommonDefines.h"
+
+// Sets default values
+ACEnemyBase::ACEnemyBase()
+{
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("MeshComp");
+	AttributeComp = CreateDefaultSubobject<UCAttributeComponent>("AttributeComp");
+
+	SetRootComponent(MeshComp);
+
+	MeshComp->SetCollisionObjectType(COLLISION_ENEMY);
+}
+void ACEnemyBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+void ACEnemyBase::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	AttributeComp->OnHealthChangedDelegate.AddDynamic(this, &ACEnemyBase::OnHealthChangedResponse);
+}
+void ACEnemyBase::OnHealthChangedResponse(float Delta, float NewHealth) 
+{
+	if (NewHealth <= 0)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, TEXT("Enemy destroyed"));
+		Destroy();
+	}
+}
+float ACEnemyBase::GetHealthPercent()
+{
+	return AttributeComp->GetHealthPercent();
+}
+
---
+++ Source/Nyx/Public/CEnemyBase.h
@@ -0,0 +1,38 @@
+// Copyright (C) 2023 - Calvin Davidson
+
+#pragma once
+
+#include "CEnemyBase.generated.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+
+class UStaticMeshComponent;
+class UCAttributeComponent;
+
+/*
+ * Basic enemy class
+ */
+UCLASS()
+class NYX_API ACEnemyBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACEnemyBase();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void PostInitializeComponents() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> MeshComp;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UCAttributeComponent> AttributeComp;
+
+	UFUNCTION()
+	void OnHealthChangedResponse(float Delta, float NewHealth);
+
+	UFUNCTION(BlueprintCallable)
+	float GetHealthPercent();
+};
