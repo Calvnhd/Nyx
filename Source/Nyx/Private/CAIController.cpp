09bb17b06a164c620139fbda7577271cc07a7080
Calvin Davidson <calvinhdavidson@gmail.com>
2023-11-01 00:48:15 +1100

    test enemies fly towards you!

--- Source/Nyx/Private/CAIController.cpp
+++ Source/Nyx/Private/CAIController.cpp
@@ -8,11 +8,7 @@ void ACAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	//	if (ensureMsgf(EnemyBehaviourTree,
-	//				   TEXT("Behaviour Tree is nullptr! Please assign EnemyBehaviourTree in your AI Controller")))
-	//	{
 	RunBehaviorTree(EnemyBaseBehaviourTree);
-	//	}
 
 	if (APawn* MyPawn = UGameplayStatics::GetPlayerPawn(this, 0))
 	{
