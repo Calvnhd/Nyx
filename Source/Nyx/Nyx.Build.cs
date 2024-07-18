92aac48d9cec7343f2d6c856a729846a6d1940fe
Calvin Davidson <82812675+Calvnhd@users.noreply.github.com>
2024-07-18 19:30:48 +1000

    Input redo.  Level redo.  Good luck. (#15)
    * enhanced input
    * new default level
    * big reset on a bunch of things

--- Source/Nyx/Nyx.Build.cs
+++ Source/Nyx/Nyx.Build.cs
@@ -8,9 +8,7 @@ public class Nyx : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTasks", "UMG"});
-
-		PrivateDependencyModuleNames.AddRange(new string[] { });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTasks", "UMG", "EnhancedInput"});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
--- Source/Nyx/Private/CPlayerCharacter.cpp
+++ Source/Nyx/Private/CPlayerCharacter.cpp
@@ -7,32 +7,18 @@
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/Controller.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Kismet/KismetMathLibrary.h"
 
-// Sets default values
 ACPlayerCharacter::ACPlayerCharacter()
 {
-	PrimaryActorTick.bCanEverTick = true;
-
-	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
-	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
-	PlayerAttributeComp = CreateDefaultSubobject<UCPlayerAttributeComponent>("PlayerAttributeComp");
-
-	SpringArmComp->SetupAttachment(RootComponent);
-	SpringArmComp->TargetArmLength = 500.0f; // The camera follows at this distance behind the character
-	SpringArmComp->SocketOffset = FVector(0, 0, 150.0f); // with this offset
-	SpringArmComp->bUsePawnControlRotation = true; // Rotate the arm based on the controller
-
-	CameraComp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
-	CameraComp->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
-
 	GetCapsuleComponent()->InitCapsuleSize(110.0f, 110.0f);
 
-	// Don't rotate when the controller rotates.
-	// Let that just affect the camera.
+	// Don't rotate when the controller rotates. Let that just affect the camera.
 	// Controller rotation usually IS the camera rotation
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationRoll = false;
@@ -40,51 +26,102 @@ ACPlayerCharacter::ACPlayerCharacter()
 
 	// Configure character movement
 	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, MovementRotationRate, 0.0f); // ...at this rotation rate
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
 
-	// This is taken from the third person example, left here as a reminder
-	// Find which variables you're using often and give them reasonable defaults
-	// GetCharacterMovement()->JumpZVelocity = 700.f;
-	// GetCharacterMovement()->AirControl = 0.35f;
-	// GetCharacterMovement()->MaxWalkSpeed = 500.f;
-	// GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
-	// GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 500.0f; // The camera follows at this distance behind the character
+	CameraBoom->SocketOffset = FVector(0, 0, 150.0f); // with this offset
+	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+
+	PlayerAttributeComp = CreateDefaultSubobject<UCPlayerAttributeComponent>(TEXT("PlayerAttributeComp"));
 }
+
 void ACPlayerCharacter::PostInitializeComponents()
 {
 	Super::PostInitializeComponents();
-
 	PlayerAttributeComp->OnHealthChangedDelegate.AddDynamic(this, &ACPlayerCharacter::OnHealthChangedResponse);
 }
+
 void ACPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	check(PlayerInputComponent);
-
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	/* Set up gameplay key bindings */
-
-	// Actions
-	PlayerInputComponent->BindAction("AttackPrimary", IE_Pressed, this, &ACPlayerCharacter::AttackPrimary);
-	PlayerInputComponent->BindAction("AttackSecondary", IE_Pressed, this, &ACPlayerCharacter::AttackSecondary);
-
-	// Movement
-	PlayerInputComponent->BindAxis("MoveForwardBackward", this, &ACPlayerCharacter::MoveForward);
-	PlayerInputComponent->BindAxis("MoveRightLeft", this, &ACPlayerCharacter::MoveRight);
-
-	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
-	// "turn" handles devices that provide an absolute delta, such as a mouse.
-	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
-	PlayerInputComponent->BindAxis("TurnMouse", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("TurnGamepad", this, &ACPlayerCharacter::TurnAtRate);
-	PlayerInputComponent->BindAxis("LookMouse", this, &APawn::AddControllerPitchInput);
-	PlayerInputComponent->BindAxis("LookGamepad", this, &ACPlayerCharacter::LookUpAtRate);
+	// Set up action bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ACPlayerCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ACPlayerCharacter::Look);
+		EnhancedInputComponent->BindAction(AttackPrimaryAction, ETriggerEvent::Triggered, this,
+										   &ACPlayerCharacter::AttackPrimary);
+		EnhancedInputComponent->BindAction(AttackSpecialAction, ETriggerEvent::Triggered, this,
+										   &ACPlayerCharacter::AttackSpecial);
+		EnhancedInputComponent->BindAction(DashAction, ETriggerEvent::Triggered, this, &ACPlayerCharacter::Dash);
+		EnhancedInputComponent->BindAction(ShieldAction, ETriggerEvent::Triggered, this, &ACPlayerCharacter::Shield);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("'%s' Failed to find an Enhanced Input component!"), *GetNameSafe(this));
+	}
 }
-void ACPlayerCharacter::HealSelf(float Amount /* = 1000 */)
+
+void ACPlayerCharacter::BeginPlay()
 {
-	PlayerAttributeComp->ApplyHealthChange(this, Amount);
+	Super::BeginPlay();
+
+	// Add Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+				ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
 }
-void ACPlayerCharacter::AttackPrimary()
+
+void ACPlayerCharacter::Move(const FInputActionValue& Value)
+{
+	// input is a Vector2D
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+		// get right vector
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		// add movement
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void ACPlayerCharacter::Look(const FInputActionValue& Value)
+{
+	// input is a Vector2D
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// add yaw and pitch input to controller
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
+
+void ACPlayerCharacter::AttackPrimary(const FInputActionValue& Value)
 {
 	// Make sure the projectile class is assigned in BP
 	if (ensureAlways(ProjectileClassPrimary))
@@ -92,24 +129,22 @@ void ACPlayerCharacter::AttackPrimary()
 		SpawnProjectile(ProjectileClassPrimary);
 	}
 }
-void ACPlayerCharacter::AttackSecondary()
+
+void ACPlayerCharacter::AttackSpecial(const FInputActionValue& Value)
 {
-	// Make sure the projectile class is assigned in BP
-	if (ensureAlways(ProjectileClassSecondary))
-	{
-		SpawnProjectile(ProjectileClassSecondary);
-	}
+	// todo
 }
-void ACPlayerCharacter::SpawnProjectile(TSubclassOf<AActor> ProjectileClass)
+
+void ACPlayerCharacter::Dash(const FInputActionValue& Value)
 {
-	FActorSpawnParameters SpawnParams;
-	// Make sure the Projectile knows that it was spawned by the Player
-	SpawnParams.Instigator = this;
-	// Make projectile always spawn at desired location, regardless of collisions
-	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
-	// Spawn projectile
-	GetWorld()->SpawnActor<AActor>(ProjectileClass, GetCrosshairTargetTM(), SpawnParams);
+	// todo
 }
+
+void ACPlayerCharacter::Shield(const FInputActionValue& Value)
+{
+	// todo
+}
+
 void ACPlayerCharacter::OnHealthChangedResponse(AActor* InstigatorActor, UCAttributeComponentBase* OwningComp,
 												float Delta, float NewHealth)
 {
@@ -120,30 +155,43 @@ void ACPlayerCharacter::OnHealthChangedResponse(AActor* InstigatorActor, UCAttri
 		DisableInput(PlayerController);
 	}
 }
+
+void ACPlayerCharacter::SpawnProjectile(TSubclassOf<AActor> ProjectileClass)
+{
+	FActorSpawnParameters SpawnParams;
+	// Make sure the Projectile knows that it was spawned by the Player
+	SpawnParams.Instigator = this;
+	// Make projectile always spawn at desired location, regardless of collisions
+	SpawnParams.SpawnCollisionHandlingOverride =
+
+		ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	// Spawn projectile
+	GetWorld()->SpawnActor<AActor>(ProjectileClass, GetCrosshairTargetTM(), SpawnParams);
+}
 FTransform ACPlayerCharacter::GetCrosshairTargetTM()
 {
 	// You want to know where you're looking from
-	FVector CameraLocation = CameraComp->GetComponentLocation();
+	FVector CameraLocation = FollowCamera->GetComponentLocation();
 	// What direction you're looking
-	FRotator CameraRotation = CameraComp->GetComponentRotation();
+	FRotator CameraRotation = FollowCamera->GetComponentRotation();
 	// What's your maximum view distance?
 	FVector ViewEnd = CameraLocation + (CameraRotation.Vector() * 10000);
-
 	// What do you see?
 	FHitResult ViewHit;
 	// This is a list of all the object types we're looking for
 	FCollisionObjectQueryParams ObjectQueryParams;
-	if (!bIsAutoAimActive)
-	{
-		ObjectQueryParams.AddObjectTypesToQuery(ECC_WorldDynamic);
-		ObjectQueryParams.AddObjectTypesToQuery(ECC_WorldStatic);
-		ObjectQueryParams.AddObjectTypesToQuery(ECC_Pawn);
-	}
+	// if (!bIsAutoAimActive)
+	//{
+	//	ObjectQueryParams.AddObjectTypesToQuery(ECC_WorldDynamic);
+	//	ObjectQueryParams.AddObjectTypesToQuery(ECC_WorldStatic);
+	//	ObjectQueryParams.AddObjectTypesToQuery(ECC_Pawn);
+	// }
 	ObjectQueryParams.AddObjectTypesToQuery(COLLISION_ENEMY);
 
 	// This is the shape of the trace.  A sphere is more lenient than a line.
 	FCollisionShape TraceShape;
-	float TraceRadius = bIsAutoAimActive ? AutoAimSweepRadius : 20.0f;
+	// float TraceRadius = bIsAutoAimActive ? AutoAimSweepRadius : 20.0f;
+	float TraceRadius = 20.0f;
 	TraceShape.SetSphere(TraceRadius);
 
 	// Ignore player
@@ -156,26 +204,25 @@ FTransform ACPlayerCharacter::GetCrosshairTargetTM()
 	// that will give you a target location
 	FVector Target = bBlockingHit ? ViewHit.ImpactPoint : ViewEnd;
 
-	//if (bBlockingHit)
+	// if (bBlockingHit)
 	//{
 	//	float Radius = 50.0f;
 	//	float Segments = 32;
 	//	float Lifetime = 5.0f;
 	//	DrawDebugSphere(GetWorld(), ViewHit.ImpactPoint, Radius, Segments, FColor::MakeRandomColor(), false, Lifetime);
-	//}
-	
-	// then you want a spawn location for the projectile
-	// todo -- make a socket on the mesh and give it a name
+	// }
+
+	// then you want a spawn location for the projectile...
 	FVector SpawnLocation = GetMuzzleLocation();
-	// and a rotation for that spawn location, looking in the direction of the target
+	// ...and a rotation for that spawn location, looking in the direction of the target
 	// Target - SpawnLocation calculates the vector from SpawnLocation to Target. This vector points from SpawnLocation
 	// towards Target.
 	FRotator SpawnRotation = UKismetMathLibrary::MakeRotFromX(Target - SpawnLocation);
 
 	// Debug info
-	FColor SightColor = bBlockingHit ? FColor::Green : FColor::Red;
-	DrawDebugLine(GetWorld(), SpawnLocation, (SpawnLocation + (SpawnRotation.Vector() * 100000)), SightColor, false,
-				  2.0f, 0, 2.0f);
+	// FColor SightColor = bBlockingHit ? FColor::Green : FColor::Red;
+	// DrawDebugLine(GetWorld(), SpawnLocation, (SpawnLocation + (SpawnRotation.Vector() * 100000)), SightColor, false,
+	//              2.0f, 0, 2.0f);
 
 	// A Transformation Matrix above the ship, looking at the target
 	return FTransform(SpawnRotation, SpawnLocation);
@@ -183,47 +230,12 @@ FTransform ACPlayerCharacter::GetCrosshairTargetTM()
 FVector ACPlayerCharacter::GetMuzzleLocation()
 {
 	// Quick n dirty for now
-	// Eventually would want to have a socket on the mesh and call something like
+	// Eventually would want to have a socket on the mesh and call something like...
 	// GetMesh()->GetSocketLocation(HandSocketName);
-	return GetCapsuleComponent()->GetComponentLocation() + FVector(0, 0, MuzzleHeightOffset);
-}
-void ACPlayerCharacter::MoveForward(float Value)
-{
-	if ((Controller != nullptr) && (Value != 0.0f))
-	{
-		// find out which way is forward
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-
-		// get forward vector
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-		AddMovementInput(Direction, Value);
-	}
+	return GetCapsuleComponent()->GetComponentLocation() + FVector(0, 0, 100);
 }
 
-void ACPlayerCharacter::MoveRight(float Value)
+void ACPlayerCharacter::HealSelf(float Amount /* = 1000 */)
 {
-	if ((Controller != nullptr) && (Value != 0.0f))
-	{
-		// find out which way is right
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-
-		// get right vector
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-		// add movement in that direction
-		AddMovementInput(Direction, Value);
-	}
-}
-
-void ACPlayerCharacter::TurnAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerYawInput(Rate * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
-}
-
-void ACPlayerCharacter::LookUpAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerPitchInput(Rate * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
+	PlayerAttributeComp->ApplyHealthChange(this, Amount);
 }
