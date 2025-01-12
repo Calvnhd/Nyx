001483527852973d7f3d9b132a3a95f5689640bf
Calvin Davidson <82812675+Calvnhd@users.noreply.github.com>
2025-01-13 00:24:50 +1100

    new TankBot class

---
+++ Source/Nyx/Public/AI/TankBot.h
@@ -0,0 +1,17 @@
+// Copyright (C) 2024 - Calvin Davidson
+
+#pragma once
+
+#include "AI/CEnemyBase.h"
+#include "CoreMinimal.h"
+
+#include "TankBot.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class NYX_API ATankBot : public ACEnemyBase
+{
+	GENERATED_BODY()
+};
